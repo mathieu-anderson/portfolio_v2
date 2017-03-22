eb3ceaaca45552e423283dae95958afe326971d9
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-03-22 14:41:46 +1300

    Basic Nuxt.js starter setup

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
     <h2 class="info" v-if="name === 'client'">
       Please refresh the page
     </h2>
-    <nuxt-link class="button" to="/About">
+    <nuxt-link class="button" to="/about">
       Home page
     </nuxt-link>
   </section>
